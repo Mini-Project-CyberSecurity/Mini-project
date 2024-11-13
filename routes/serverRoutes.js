f12db5c9247844a2d995fb9ee751eb738855f17c
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2024-11-13 17:43:30 +0530

    Delete routes directory

--- routes/serverRoutes.js
+++
@@ -1,9 +0,0 @@
-// serverRoutes.js
-const express = require("express")
-const { getServerStatus } = require("../controllers/serverController")
-
-const router = express.Router()
-
-router.get("/server-status", getServerStatus)
-
-module.exports = router
